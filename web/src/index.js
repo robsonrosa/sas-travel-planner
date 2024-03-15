926ac5a093cea5efa98294186c862924c71c224a
Robson Rosa <codebrain.robson.rosa@kiwigrid.com>
2024-03-15 11:00:07 -0300

    remove api mock in web application

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SearchPage from '@pages/SearchPage';
-import { setupMockSSE } from '@utils/MockEventSource';
-
-if (process.env.NODE_ENV === 'development') {
-  setupMockSSE(2000);
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
